f6beea4a79cb8ec089b56ebc63542b18a10f9a53
Harish1230 <hharish270@gmail.com>
2024-04-21 23:32:21 -0500

    Bug Fixes with Routing

--- Front End/Originals/src/App.js
+++ Front End/Originals/src/App.js
@@ -1,25 +1,40 @@
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Tours from './components/Tours';
 import Motels from './components/Motels';
 import PlacesToVisit from './components/PlacesToVisit';
 import Restaurants from './components/Restaurants';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import NoMatch from './components/NoMatch';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/tours" component={<Tours/>} />
-        <Route path="/motels" component={Motels} />
-        <Route path="/places-to-visit" component={PlacesToVisit} />
-        <Route path="/restaurants" component={Restaurants} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
-      </Routes>
-  </Router>
-  );
+    return (
+        <Router>
+            <div className="App">
+
+                <nav>
+                    <Link to="/tours"><button>Tours</button></Link>
+                    <Link to="/motels"><button>Motels</button></Link>
+                    <Link to="/places-to-visit"><button>Places to Visit</button></Link>
+                    <Link to="/restaurants"><button>Restaurants</button></Link>
+                    <Link to="/signin"><button>Sign In</button></Link>
+                    <Link to="/signup"><button>Sign Up</button></Link>
+                </nav>
+
+                <Routes>
+                    <Route path="/tours" element={<Tours />} />
+                    <Route path="/motels" element={<Motels />} />
+                    <Route path="/places-to-visit" element={<PlacesToVisit />} />
+                    <Route path="/restaurants" element={<Restaurants />} />
+                    <Route path="/signin" element={<SignIn />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="*" element={<NoMatch/>} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ Front End/Originals/src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NoMatch() {
+    return (
+        <div>
+            {/* Sign-up form */}
+            <h1>No Route Found</h1>
+        </div>
+    );
+}
+
+export default NoMatch;
