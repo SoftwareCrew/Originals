8fdb2f6201bd3563a26a94da93bf5379f13d9076
vijaya295 <107910026+vijaya295@users.noreply.github.com>
2024-04-22 16:55:57 -0500

    Login

--- Front End/Originals/src/components/SignIn.js
+++ Front End/Originals/src/components/SignIn.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleLogin = () => {
-    console.log("Login clicked!");
+    if (username === "yourUsername" && password === "yourPassword") {
+      console.log("Login successful!");
+      // Perform further actions like redirecting to another page
+    } else {
+      console.log("Invalid credentials!");
+      // Display an error message to the user
+    }
   };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div>
       {/* Sign-in form */}
       <h1>Sign In</h1>
+      <div>
+        <label>Username:</label>
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
       <button onClick={handleLogin}>Login</button>
     </div>
   );
